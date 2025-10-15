204b7905a82d4429ee69ff5e456e31b8cfadc263
Username-Red <ashkestrel@gmail.com>
2025-10-15 19:07:32 +0200

    adding image functionality

--- app/components/ProductsList.tsx
+++ app/components/ProductsList.tsx
@@ -11,7 +11,7 @@ const ProductsList = async () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
       {products.map((product) => (
-        <ProductCard key={product.id} name={product.name} price={product.price} image={"images/placeholder.webp"} />
+        <ProductCard key={product.id} name={product.name} price={product.price} image={product.image} />
       ))}
     </div>
   );
